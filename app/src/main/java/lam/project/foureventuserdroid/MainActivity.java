c8dd224912fcfc2e1fa6f2640839115b936f8d22
Gianmarco Spinaci <spino9330@gmail.com>
2016-07-30 12:23:49 +0200

    Mostro l'activity di registrazione se non trovo User nelle SharedPreferences

--- app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
+++ app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
@@ -1,8 +1,11 @@
 package lam.project.foureventuserdroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import lam.project.foureventuserdroid.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Controllo se mostrare la registrazione
+        final User user = UserManager.get(this).getUser();
+        if(user == null){
+
+            Intent intent = new Intent(this,RegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
     private View.OnClickListener buttonListener = new View.OnClickListener(){
