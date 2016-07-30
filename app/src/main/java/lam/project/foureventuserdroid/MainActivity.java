ef564398da16f153612986aac5d507664d7ee903
Valentina <94tinat@gmail.com>
2016-07-30 11:24:48 +0200

    Aggiunte icone in drawable + in layout registrazione

--- app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
+++ app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_registration);
+        setContentView(R.layout.activity_main);
     }
 }
