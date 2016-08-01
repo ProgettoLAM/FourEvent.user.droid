94753289374e28bab8786061d8f7c8061f28106a
Gianmarco Spinaci <spino9330@gmail.com>
2016-08-01 12:15:51 +0200

    Aggiunta SplashActivity
    Controlla se l'activity successiva è quella della registrazione o è quella dell'applicazione

---
+++ app/src/main/java/lam/project/foureventuserdroid/SplashActivity.java
@@ -0,0 +1,66 @@
+package lam.project.foureventuserdroid;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import lam.project.foureventuserdroid.model.User;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long MIN_WAIT_INTERVAL = 1500L;
+    private static final long MAX_WAIT_INTERVAL = 3000L;
+    private static final int GO_AHEAD_WHAT = 1;
+
+    private long mStartTime;
+    private boolean mIsDone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        final ImageView logoImageView = (ImageView) findViewById(R.id.splash_imageview);
+        assert logoImageView != null;
+        logoImageView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                long elapsedTime = SystemClock.uptimeMillis() - mStartTime;
+                if(elapsedTime >= MIN_WAIT_INTERVAL && !mIsDone){
+
+                    mIsDone = true;
+                    goAhead();
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void goAhead() {
+
+        boolean exist = UserManager.get(this).getUser() != null;
+
+        Intent intent;
+
+        if(exist)
+            intent = new Intent(this,MainActivity.class);
+        else
+            intent = new Intent(this,RegistrationActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+}
