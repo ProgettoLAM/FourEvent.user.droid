1b85899f0a894e4e5c12957ee3139c09a2626f54
Valentina <94tinat@gmail.com>
2016-08-01 18:59:24 +0200

    Aggiunte classi per Volley

---
+++ app/src/main/java/lam/project/foureventuserdroid/utils/VolleyRequest.java
@@ -0,0 +1,47 @@
+package lam.project.foureventuserdroid.utils;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Vale on 01/08/2016.
+ */
+
+public class VolleyRequest {
+
+    private static RequestQueue mRequestQueue;
+    private static VolleyRequest instance;
+    private static Context context;
+
+    private VolleyRequest(Context context) {
+        this.context = context;
+
+        if(mRequestQueue == null) {
+            mRequestQueue = initRequestQueue();
+            mRequestQueue.start();
+        }
+    }
+
+    private RequestQueue initRequestQueue() {
+        return Volley.newRequestQueue(context);
+    }
+
+    //Singleton, si crea una sola istanza della classe
+    public static VolleyRequest get(Context context) {
+        if(instance == null) {
+            instance = new VolleyRequest(context);
+        }
+        return instance;
+
+    }
+    public void add(Request request) {
+        mRequestQueue.add(request);
+    }
+
+    public interface QueueProvider {
+        RequestQueue getRequestQueue();
+    }
+}
