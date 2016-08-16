2c020aa6fabc2a5afc7e8d0adfc93b6b27a0d19b
Gianmarco Spinaci <spino9330@gmail.com>
2016-08-16 19:34:42 +0200

    completata gestione errori GooglePlayServices Client

--- app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
+++ app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
@@ -1,36 +1,35 @@
 package lam.project.foureventuserdroid;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.os.AsyncTask;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-
-
-import java.io.IOException;
-import java.util.List;
 
 import lam.project.foureventuserdroid.complete_profile.StepManager;
 import lam.project.foureventuserdroid.complete_profile.CompleteProfileActivity;
@@ -40,19 +39,29 @@ import lam.project.foureventuserdroid.fragment.ParticipationFragment;
 import lam.project.foureventuserdroid.fragment.ProfileFragment;
 import lam.project.foureventuserdroid.fragment.SettingsFragment;
 import lam.project.foureventuserdroid.fragment.WalletFragment;
-import lam.project.foureventuserdroid.model.User;
-import lam.project.foureventuserdroid.utils.shared_preferences.UserManager;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,
-        GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks,
-        LocationListener {
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final int REQUEST_RESOLVE_ERROR = 0;
-    private GoogleApiClient googleApiClient;
-    private boolean resolvingError;
-    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+
+    private GoogleApiClient.ConnectionCallbacks mConnectionCallbacks;
+    private GoogleApiClient.OnConnectionFailedListener mOnConnectionFailedListener;
+
+    final static String DIALOG_ERROR_TAG = "DIALOG_ERROR_TAG";
+
+    private static final int REQUEST_RESOLVE_ERROR = 2;
+
+    private static final String RESOLVING_ERROR_STATE_KEY = "RESOLVING_ERROR_STATE_KEY";
+
+    private boolean mResolvingError;
+
+    public interface ConnectionCallbacks{
+
+        void onConnected(Bundle connectionHint);
+        void onConnectionSuspended(int cause);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,14 +69,8 @@ public class MainActivity extends AppCompatActivity
 
         //se il profilo è completo
         if (StepManager.get(this).getStep() == StepManager.COMPLETE) {
-            setContentView(R.layout.activity_main);
 
-            //Istanziare un oggetto GoogleApiClient
-            googleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(LocationServices.API)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
+            setContentView(R.layout.activity_main);
 
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
             toolbar.setTitle(R.string.title_events);
@@ -82,10 +85,70 @@ public class MainActivity extends AppCompatActivity
             NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
             navigationView.setNavigationItemSelectedListener(this);
 
-            //Setto la pagina principale come quella di ricerca degli eventi
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.anchor_point, new EventsFragment())
-                    .commit();
+            if(savedInstanceState == null){
+
+                //Setto la pagina principale come quella di ricerca degli eventi
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.anchor_point, new EventsFragment())
+                        .commit();
+            }else{
+
+                mResolvingError = savedInstanceState.getBoolean(RESOLVING_ERROR_STATE_KEY,false);
+            }
+
+
+            mConnectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
+
+                int CAUSE_SERVICE_DISCONNECTED = 1;
+                int CAUSE_NETWORK_LOST = 2;
+
+                @Override
+                public void onConnected(Bundle connectionHint) {
+
+                }
+
+                @Override
+                public void onConnectionSuspended(int cause) {
+
+                }
+            };
+
+            mOnConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
+
+                @Override
+                public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    if(mResolvingError){
+
+                        return;
+                    }
+
+                    else if (connectionResult.hasResolution()) {
+
+                        try{
+
+                            mResolvingError = true;
+                            connectionResult.startResolutionForResult(MainActivity.this,
+                                    REQUEST_RESOLVE_ERROR);
+
+                        } catch (IntentSender.SendIntentException e) {
+
+                            mGoogleApiClient.connect();
+                        }
+                    }
+
+                    else {
+
+                        mResolvingError = true;
+                    }
+                }
+            };
+
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(mConnectionCallbacks)
+                    .addOnConnectionFailedListener(mOnConnectionFailedListener)
+                    .build();
 
         } else {
 
@@ -173,151 +236,65 @@ public class MainActivity extends AppCompatActivity
                 .commit();
     }
 
-    private View.OnClickListener buttonListener = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-
-            int itemId = v.getId();
-
-            switch (itemId) {
-
-                default:
-                    break;
-            }
-
-        }
-    };
-
-    //Avviare la connessione dai Google Play Services
     @Override
     protected void onStart() {
         super.onStart();
-        if (!resolvingError) {
-            googleApiClient.connect();
+
+        if(!mResolvingError){
+
+            mGoogleApiClient.connect();
         }
     }
 
-    //Stoppare la connessione dai Google Play Services
     @Override
     protected void onStop() {
 
-        if (googleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
-            googleApiClient.disconnect();
-        }
+        mGoogleApiClient.disconnect();
         super.onStop();
     }
 
-    /*Per sapere con certezza che la connessione ai Google Play Services è stata attivata
-      ed avviare la ricezione di informazioni di localizzazione da parte del provider
-     */
-    @Override
-    public void onConnected(Bundle arg0) {
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setSmallestDisplacement(5);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    public static class ErrorDialogFragment extends DialogFragment{
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        public ErrorDialogFragment() {}
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR_TAG);
+
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(),errorCode,REQUEST_RESOLVE_ERROR
+            );
         }
-        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((MainActivity) getActivity()).onDialogDismissed();
+        }
     }
 
+    private void onDialogDismissed() {}
+
     @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        if (resolvingError)
-            return;
-        if (result.hasResolution()) {
-            try {
-                resolvingError = true;
-                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
-            }
-            catch (IntentSender.SendIntentException e)
-            {
-                googleApiClient.connect();
-            }
-        } else {
-            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),this, REQUEST_RESOLVE_ERROR).show();
-            resolvingError = true;
-        }
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putBoolean(RESOLVING_ERROR_STATE_KEY,mResolvingError);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_RESOLVE_ERROR) {
-            resolvingError = false;
-            if (resultCode == RESULT_OK)
-            {
-                if (!googleApiClient.isConnecting() &&
-                        !googleApiClient.isConnected()) {
-                    googleApiClient.connect();
+        if(requestCode == REQUEST_RESOLVE_ERROR){
+
+            mResolvingError = false;
+
+            if(resultCode == RESULT_OK) {
+
+                if(!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()){
+
+                    mGoogleApiClient.connect();
                 }
             }
         }
     }
-
-    //Gestione delle informazioni di localizzazione
-    @Override
-    public void onLocationChanged(final Location location) {
-        double latitude = location.getLatitude();
-        updateText(R.id.latitude, String.valueOf(latitude));
-        double longitude = location.getLongitude();
-        updateText(R.id.longitude, String.valueOf(longitude));
-
-        //Metodo per evitare che i tempi di latenza bloccano l'Activity
-        new AsyncTask<Void,Void,String>()
-        {
-            @Override
-            protected String doInBackground(Void... voids)
-            {
-                //Per convertire le coordinate in informazioni stradali
-                Geocoder coder = new Geocoder(MainActivity.this);
-                try {
-                    List<Address> l = coder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                    if (l.size()>0)
-                        return l.get(0).getLocality();
-                } catch (IOException e) {
-                    return null;
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-                String location;
-                String name;
-
-                if (result!=null)
-                    updateText(R.id.location, result);
-                else {
-                    location = UserManager.get().getUser().location;
-                    updateText(R.id.location, location);
-                }
-
-                name = UserManager.get().getUser().name;
-                updateText(R.id.name, name);
-
-            }
-        }.execute();
-    }
-
-    private void updateText(int id, String text)
-    {
-        TextView textView = (TextView) findViewById(id);
-        textView.setText(text);
-    }
-
-
-    @Override
-    public void onConnectionSuspended(int i) {}
-
 }
