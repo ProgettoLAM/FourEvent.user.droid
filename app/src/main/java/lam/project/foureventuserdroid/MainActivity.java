91e155bcbe7a43ebe24b697ec3f78d2966665c59
Valentina <94tinat@gmail.com>
2016-08-16 16:24:54 +0200

    Aggiunta localizzazione GPS + visualizzazione nome e località nella navigation view dell'user salvato

--- app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
+++ app/src/main/java/lam/project/foureventuserdroid/MainActivity.java
@@ -1,9 +1,14 @@
 package lam.project.foureventuserdroid;
 
-import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.support.design.widget.NavigationView;
-import android.support.multidex.MultiDex;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,8 +16,21 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+
+import java.io.IOException;
+import java.util.List;
 
 import lam.project.foureventuserdroid.complete_profile.StepManager;
 import lam.project.foureventuserdroid.complete_profile.CompleteProfileActivity;
@@ -22,11 +40,19 @@ import lam.project.foureventuserdroid.fragment.ParticipationFragment;
 import lam.project.foureventuserdroid.fragment.ProfileFragment;
 import lam.project.foureventuserdroid.fragment.SettingsFragment;
 import lam.project.foureventuserdroid.fragment.WalletFragment;
-import lam.project.foureventuserdroid.fragment.eventFragment.AllEventsFragment;
 import lam.project.foureventuserdroid.model.User;
+import lam.project.foureventuserdroid.utils.shared_preferences.UserManager;
+
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks,
+        LocationListener {
+
+    private static final int REQUEST_RESOLVE_ERROR = 0;
+    private GoogleApiClient googleApiClient;
+    private boolean resolvingError;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +61,13 @@ public class MainActivity extends AppCompatActivity
         //se il profilo è completo
         if (StepManager.get(this).getStep() == StepManager.COMPLETE) {
             setContentView(R.layout.activity_main);
+
+            //Istanziare un oggetto GoogleApiClient
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
 
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
             toolbar.setTitle(R.string.title_events);
@@ -54,10 +87,9 @@ public class MainActivity extends AppCompatActivity
                     .replace(R.id.anchor_point, new EventsFragment())
                     .commit();
 
-        }
-        else{
+        } else {
 
-            Intent completeProfileIntent = new Intent(this,CompleteProfileActivity.class);
+            Intent completeProfileIntent = new Intent(this, CompleteProfileActivity.class);
             startActivity(completeProfileIntent);
             finish();
         }
@@ -83,12 +115,12 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void showFragment(final MenuItem menuItem){
+    private void showFragment(final MenuItem menuItem) {
 
         final int itemId = menuItem.getItemId();
         final Fragment nextFragment;
 
-        switch (itemId){
+        switch (itemId) {
 
             case R.id.nav_events:
 
@@ -132,7 +164,8 @@ public class MainActivity extends AppCompatActivity
                 nextFragment = new EventsFragment();
                 break;
 
-            default: throw new IllegalArgumentException("No fragment given");
+            default:
+                throw new IllegalArgumentException("No fragment given");
         }
 
         getSupportFragmentManager().beginTransaction()
@@ -140,19 +173,151 @@ public class MainActivity extends AppCompatActivity
                 .commit();
     }
 
-    private View.OnClickListener buttonListener = new View.OnClickListener(){
+    private View.OnClickListener buttonListener = new View.OnClickListener() {
 
         @Override
         public void onClick(View v) {
 
             int itemId = v.getId();
 
-            switch (itemId){
+            switch (itemId) {
 
-                default: break;
+                default:
+                    break;
             }
 
         }
     };
+
+    //Avviare la connessione dai Google Play Services
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!resolvingError) {
+            googleApiClient.connect();
+        }
+    }
+
+    //Stoppare la connessione dai Google Play Services
+    @Override
+    protected void onStop() {
+
+        if (googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    /*Per sapere con certezza che la connessione ai Google Play Services è stata attivata
+      ed avviare la ricezione di informazioni di localizzazione da parte del provider
+     */
+    @Override
+    public void onConnected(Bundle arg0) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setSmallestDisplacement(5);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (resolvingError)
+            return;
+        if (result.hasResolution()) {
+            try {
+                resolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            }
+            catch (IntentSender.SendIntentException e)
+            {
+                googleApiClient.connect();
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),this, REQUEST_RESOLVE_ERROR).show();
+            resolvingError = true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            resolvingError = false;
+            if (resultCode == RESULT_OK)
+            {
+                if (!googleApiClient.isConnecting() &&
+                        !googleApiClient.isConnected()) {
+                    googleApiClient.connect();
+                }
+            }
+        }
+    }
+
+    //Gestione delle informazioni di localizzazione
+    @Override
+    public void onLocationChanged(final Location location) {
+        double latitude = location.getLatitude();
+        updateText(R.id.latitude, String.valueOf(latitude));
+        double longitude = location.getLongitude();
+        updateText(R.id.longitude, String.valueOf(longitude));
+
+        //Metodo per evitare che i tempi di latenza bloccano l'Activity
+        new AsyncTask<Void,Void,String>()
+        {
+            @Override
+            protected String doInBackground(Void... voids)
+            {
+                //Per convertire le coordinate in informazioni stradali
+                Geocoder coder = new Geocoder(MainActivity.this);
+                try {
+                    List<Address> l = coder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if (l.size()>0)
+                        return l.get(0).getLocality();
+                } catch (IOException e) {
+                    return null;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                String location;
+                String name;
+
+                if (result!=null)
+                    updateText(R.id.location, result);
+                else {
+                    location = UserManager.get().getUser().location;
+                    updateText(R.id.location, location);
+                }
+
+                name = UserManager.get().getUser().name;
+                updateText(R.id.name, name);
+
+            }
+        }.execute();
+    }
+
+    private void updateText(int id, String text)
+    {
+        TextView textView = (TextView) findViewById(id);
+        textView.setText(text);
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {}
 
 }
